9da5714d3f97593c9d548892c93839dcca9f4428
dasimems <isaacseun63@gmail.com>
2023-02-17 10:28:57 -0800

    done with the file input validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -86,6 +86,16 @@ function showFormErr(dataName, message, removeErr){
             if(inp.getAttribute("data-name") === dataName){
                 inp.classList.remove("input-err");
             }
+
+            if(inp.getAttribute("type") === "file"){
+
+                formLabels.forEach(label => {
+                    if(label.getAttribute("data-name") === dataName){
+                        label.innerText = message;
+                    }
+                })
+
+            }
         })
     
         formLabels.forEach(inp => {
@@ -110,14 +120,13 @@ function showFormErr(dataName, message, removeErr){
 function submitFormValidation(form){
     
     if(form){
+        
         var inputs = [...form.target.elements];
         var action = form.target.getAttribute("action");
         var method = form.target.getAttribute("method");
         var data = {};
         var checkArr = [];
         
-                    
-        form.preventDefault();
 
         inputs.forEach(inputElement => {
 
@@ -125,6 +134,9 @@ function submitFormValidation(form){
             var regex = inputElement.getAttribute("data-regex")
             var dataMin = inputElement.getAttribute("data-min")
             var generalErr = inputElement.getAttribute("data-name-err")
+            var dataMaxSize = inputElement.getAttribute("data-max-size")
+            var dataMinSize = inputElement.getAttribute("data-min-size")
+            var dataType = inputElement.getAttribute("data-type")
             var dataMax = inputElement.getAttribute("data-max")
             var dataRequired = inputElement.getAttribute("data-required") || inputElement.getAttribute("required")
             var message = "";
@@ -137,7 +149,7 @@ function submitFormValidation(form){
 
                 var val = inputElement.value.trim();
 
-                if(val === "" || (regex && regex !== "" && !new RegExp(regex).test(val)) || (dataMin && dataMin !== "" && val.length < parseInt(dataMin)) || (dataMax && dataMax !== "" && val.length > parseInt(dataMax))){
+                if((dataRequired && val === "") || (regex && regex !== "" && !new RegExp(regex).test(val)) || (dataMin && dataMin !== "" && val.length < parseInt(dataMin)) || (dataMax && dataMax !== "" && val.length > parseInt(dataMax))){
 
                     if(dataRequired && val === ""){
     
@@ -152,8 +164,6 @@ function submitFormValidation(form){
                         }
     
                     }else if(regex && regex !== "" && !new RegExp(regex).test(val)){
-
-                        console.log("worked")
     
                         var newErr = inputElement.getAttribute("data-regex-err")
     
@@ -205,52 +215,193 @@ function submitFormValidation(form){
                     checkArr.push(true);
                     showFormErr(dataName, "", true)
                     data[dataName] = val;
+
+                    if(checkArr.length === inputs.length && !checkArr.includes(false)){
+
+                        form.returnValue  = true;
+                        form.defaultPrevented  = false;
+
+                    }
                 }
             
+            }else{
+
+                var fileTypePassed = false,
+                    maxSizePassed = false,
+                    minSizePassed = false,
+                    type = "",
+                    fileLengthExceeded = false,
+                    neededDataTypes = [];
+
+
+                if(inputElement.getAttribute("type") === "file"){
+                    type = "file";
+
+                    if(dataMax && dataMax !== ""){
+
+                        if(inputElement.files.length > dataMax){
+
+                            fileLengthExceeded = true
+
+                        }
+                    }
+
+                    if(dataType && dataType !== ""){
+
+                        var checkDataType = (dataType.replace(/\[|\"|\'|\]/g,'').split(',')).map(data => data.trim());
+                        
+                        var uploadedDataTypes = ([...inputElement.files].map(file => file.type))
+
+                        uploadedDataTypes.forEach(data => {
+                            if(checkDataType.includes(data)){
+                                neededDataTypes.push(true)
+                            }else{
+                                neededDataTypes.push(false)
+                            }
+                        })
+
+
+                        if(uploadedDataTypes.length === neededDataTypes.length && !neededDataTypes.includes(false)){
+                            fileTypePassed = true;
+                        }
+
+                        
+
+                    }
+
+                    
+                    if(dataMaxSize && dataMaxSize !== ""){
+
+                        dataMaxSize = (dataMaxSize * 1000000)
+
+
+                         var uploadedDataSizes = ([...inputElement.files].map(file => file.size));
+
+                         var outlaws = uploadedDataSizes.filter(sizes => sizes > dataMaxSize);
+
+
+                         if(outlaws.length > 0){
+                            maxSizePassed = true
+                         }
+
+                    }
+
+                    if(dataMinSize && dataMinSize !== ""){
+
+                         dataMinSize = (dataMinSize * 1000000)
+
+                         var uploadedDataSizes = ([...inputElement.files].map(file => file.size));
+
+                         var outlaws = uploadedDataSizes.filter(sizes => sizes < dataMinSize);
+
+                         if(outlaws.length > 0){
+                            minSizePassed = true
+                         }
+
+
+                    }
+
+                }
+
+                var val = inputElement.value.trim();
+
+                if((dataRequired && val === "") || (type === "file" && fileLengthExceeded) || (dataMaxSize && dataMaxSize !== "" && maxSizePassed) || (dataMinSize && dataMinSize !== "" && minSizePassed) || (dataType && dataType !== "" && !fileTypePassed)){
+                    
+
+                    if(dataRequired && val === ""){
+    
+                        var newErr = inputElement.getAttribute("data-required-err") || inputElement.getAttribute("required-err")
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "This field is required";
+                        }
+    
+                    }else if(type === "file" && fileLengthExceeded){
+
+                        var newErr = inputElement.getAttribute("data-max-err")
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "You can't select more than " + dataMax + " files";
+                        }
+
+                    }else if(dataType && dataType !== "" && !fileTypePassed){
+
+                        var newErr = inputElement.getAttribute("data-type-err");
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "The file selected is an invalid file format";
+                        }
+
+                        
+                    }else if(dataMaxSize && dataMaxSize !== "" && maxSizePassed){
+
+                        var newErr = inputElement.getAttribute("data-max-size-err");
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "The file selected too large";
+                        }
+
+
+                    }else if(dataMinSize && dataMinSize !== "" && minSizePassed){
+
+                        var newErr = inputElement.getAttribute("data-min-size-err");
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "The file selected too small";
+                        }
+
+
+                    }
+
+                    
+                    showFormErr(dataName, message)
+
+                    if(inputElement.tagName.toLowerCase() !== "button" && inputElement.type !== "submit"){
+
+                        checkArr.push(false);
+                    }else{
+                        
+                        checkArr.push(true);
+                    }
+                            
+                            
+                }else{
+                    
+                    checkArr.push(true);
+                    showFormErr(dataName, "", true)
+                    data[dataName] = val;
+
+                    
+                }
+
             }
 
         })
 
-        if(!checkArr.includes(false)){
 
-            var xmlHttp;
-
-            if (window.XMLHttpRequest) {
-            // code for modern browsers
-                xmlHttp = new XMLHttpRequest();
-            } else {
-            // code for old IE browsers
-                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-
-            if(action && action !== ""){
-
-                if(!method || method === ""){
-                    method = "get";
-                }
-
-                xmlHttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-
-                        
-
-                        inputs.forEach((element)=>{
-
-                            element.value = "";
-
-                        })
-
-
-                    }else{
-                        
-                    }
-                };
-
-                xmlHttp.open(method, action, true)
-
-                xmlHttp.send();
-            }
-
+        if(checkArr.includes(false)){
+                    
+            form.preventDefault();
 
         }
     }
@@ -267,9 +418,15 @@ function addInputEventListener(){
         var regex = inputElement.getAttribute("data-regex")
         var dataMin = inputElement.getAttribute("data-min")
         var generalErr = inputElement.getAttribute("data-name-err")
+        var dataMaxSize = inputElement.getAttribute("data-max-size")
+        var dataMinSize = inputElement.getAttribute("data-min-size")
+        var dataType = inputElement.getAttribute("data-type")
         var dataMax = inputElement.getAttribute("data-max")
         var dataRequired = inputElement.getAttribute("data-required") || inputElement.getAttribute("required")
         var message = "";
+
+        
+
 
         if(generalErr && generalErr !== ""){
             message = generalErr;
@@ -281,7 +438,7 @@ function addInputEventListener(){
 
                 var val = e.target.value.trim();
 
-                if(val === "" || (regex && regex !== "" && !new RegExp(regex).test(val)) || (dataMin && dataMin !== "" && val.length < parseInt(dataMin)) || (dataMax && dataMax !== "" && val.length > parseInt(dataMax))){
+                if((dataRequired && val === "") || (regex && regex !== "" && !new RegExp(regex).test(val)) || (dataMin && dataMin !== "" && val.length < parseInt(dataMin)) || (dataMax && dataMax !== "" && val.length > parseInt(dataMax))){
 
                     if(dataRequired && val === ""){
     
@@ -351,6 +508,179 @@ function addInputEventListener(){
 
             inputElement.addEventListener("change", (e)=>{
 
+                var fileTypePassed = false,
+                    maxSizePassed = false,
+                    minSizePassed = false,
+                    type = "",
+                    fileLengthExceeded = false,
+                    neededDataTypes = [],
+                    fileLength= 0;
+
+
+                if(inputElement.getAttribute("type") === "file"){
+                    type = "file";
+                    
+                    fileLength = inputElement.files.length;
+
+                    if(dataMax && dataMax !== ""){
+
+                        if(inputElement.files.length > dataMax){
+
+                            fileLengthExceeded = true
+                            fileLength = 0;
+
+                        }
+                    }
+
+                    if(dataType && dataType !== ""){
+
+                        var checkDataType = (dataType.replace(/\[|\"|\'|\]/g,'').split(',')).map(data => data.trim());
+                        
+                        var uploadedDataTypes = ([...inputElement.files].map(file => file.type))
+
+                        uploadedDataTypes.forEach(data => {
+                            if(checkDataType.includes(data)){
+                                neededDataTypes.push(true)
+                            }else{
+                                neededDataTypes.push(false)
+                            }
+                        })
+
+
+                        if(uploadedDataTypes.length === neededDataTypes.length && !neededDataTypes.includes(false)){
+                            fileTypePassed = true;
+                        }
+
+                        
+
+                    }
+
+                    if(dataMaxSize && dataMaxSize !== ""){
+
+                        dataMaxSize = (dataMaxSize * 1000000)
+
+
+                         var uploadedDataSizes = ([...inputElement.files].map(file => file.size));
+
+
+                         var outlaws = uploadedDataSizes.filter(sizes => sizes > dataMaxSize);
+
+
+
+                         if(outlaws.length > 0){
+                            maxSizePassed = true
+                         }
+
+                    }
+
+                    if(dataMinSize && dataMinSize !== ""){
+
+                         dataMinSize = (dataMinSize * 1000000)
+
+                         var uploadedDataSizes = ([...inputElement.files].map(file => file.size));
+
+                         var outlaws = uploadedDataSizes.filter(sizes => sizes < dataMinSize);
+
+                         if(outlaws.length > 0){
+                            minSizePassed = true
+                         }
+
+
+                    }
+
+                }
+
+                
+                var val = e.target.value.trim();
+
+                if((dataRequired && val === "") || (type === "file" && fileLengthExceeded) || (dataMaxSize && dataMaxSize !== "" && maxSizePassed) || (dataMinSize && dataMinSize !== "" && minSizePassed) || (dataType && dataType !== "" && !fileTypePassed)){
+
+                    if(dataRequired && val === ""){
+    
+                        var newErr = inputElement.getAttribute("data-required-err") || inputElement.getAttribute("required-err")
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "This field is required";
+                        }
+    
+                    }else if(type === "file" && fileLengthExceeded){
+
+                        var newErr = inputElement.getAttribute("data-max-err")
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "You can't select more than " + dataMax + " files";
+                        }
+
+                    }else if(dataType && dataType !== "" && !fileTypePassed){
+
+                        var newErr = inputElement.getAttribute("data-type-err");
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "The file selected is an invalid file format";
+                        }
+
+                        
+                    }else if(dataMaxSize && dataMaxSize !== "" && maxSizePassed){
+
+                        var newErr = inputElement.getAttribute("data-max-size-err");
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "The file selected too large";
+                        }
+
+
+                    }else if(dataMinSize && dataMinSize !== "" && minSizePassed){
+
+                        var newErr = inputElement.getAttribute("data-min-size-err");
+    
+                        if(newErr && newErr !== ""){
+    
+                            message = newErr;
+    
+                        }else{
+                            message = "The file selected too small";
+                        }
+
+
+                    }
+
+
+    
+                    showFormErr(dataName, message);
+                    inputElement.value = ""
+
+                }else{
+
+                    var mess = fileLength + (fileLength > 1?" files": " file") + " selected";
+                    
+                    if(fileLength > 0){
+
+                        showFormErr(dataName, mess, true)
+                    }else{
+                        
+                        showFormErr(dataName, "", true)
+                    }
+
+                    return true;
+                    
+                }
+
             })
 
         }
@@ -375,7 +705,7 @@ window.addEventListener("load", () => {
     addInputEventListener();
 
     forms.forEach(form => {
-        form.addEventListener("submit", submitFormValidation);
+        form.addEventListener("submit", submitFormValidation, true);
     })
 
 })
